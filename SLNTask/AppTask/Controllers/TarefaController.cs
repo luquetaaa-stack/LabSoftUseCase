4f93aeaaa2e5acbc420532db4295d435046bada1
Antônio Lucas <al8978161@gmail.com>
2026-08-07 16:45:46 -0300

    commit sobre

--- SLNTask/AppTask/Controllers/TarefaController.cs
+++ SLNTask/AppTask/Controllers/TarefaController.cs
@@ -25,6 +25,11 @@ namespace AppTask.Controllers
             return View(await dbTasksContext.ToListAsync());
         }
 
+        public async Task<IActionResult> Sobre()
+        {
+            return View();
+        }
+
         // GET: Tarefa/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -47,7 +52,7 @@ namespace AppTask.Controllers
         // GET: Tarefa/Create
         public IActionResult Create()
         {
-            ViewData["CodigoFuncionario"] = new SelectList(_context.Funcionarios, "Codigo", "Nome");
+            ViewData["ListaFuncionario"] = new SelectList(_context.Funcionarios, "Codigo", "Nome");
             return View();
         }
 
@@ -56,7 +61,7 @@ namespace AppTask.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Codigo,Descricao,DataPlanejada,DataIniciada,DataFinalizada,DataCancelada,StatusTarefa,Prazo,CodigoFuncionario")] Tarefa tarefa)
+        public async Task<IActionResult> Create([Bind("Codigo,Descricao,DataPlanejada,DataIniciada,DataFinalizada,DataCancelada,StatusTarefa,Prazo,FuncionarioId")] Tarefa tarefa)
         {
             if (ModelState.IsValid)
             {
---
+++ SLNTask/AppTask/Views/Tarefa/Sobre.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppTask.Views.Tarefa
+{
+    public class SobreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
