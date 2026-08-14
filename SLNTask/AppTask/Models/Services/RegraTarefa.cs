a0d34626fcb9420c112aee5155d113df9ddeb6a3
antonio lucas <al8978161@gmail.com>
2026-08-14 16:38:05 -0300

    finalizado

---
+++ SLNTask/AppTask/Models/Services/RegraTarefa.cs
@@ -0,0 +1,12 @@
+﻿using AppTask.Models.interfaces;
+
+namespace AppTask.Models.Services
+{
+    public class RegraTarefa : IRegraTarefa
+    {
+        public bool ValidarDataFinal(DateTime? DataIniciada, DateTime? DataFinalizada)
+        {
+            return DataIniciada>=DataFinalizada;
+        }
+    }
+}
---
+++ SLNTask/AppTask/Models/interfaces/IRegraTarefa.cs
@@ -0,0 +1,7 @@
+﻿namespace AppTask.Models.interfaces
+{
+    public interface IRegraTarefa
+    {
+        bool ValidarDataFinal(DateTime? datainicial, DateTime? datafinal);
+    }
+}
